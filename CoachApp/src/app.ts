bf18cd543053745beeb5a5a62076b2eec1934709
ksoohoo <kimi.shirasaki@nowintelligence.com>
2018-11-27 15:10:59 +0900

    Basic navigation routes

--- CoachApp/src/app.ts
+++ CoachApp/src/app.ts
@@ -1,16 +1,20 @@
-import { Router, RouterConfiguration } from 'aurelia-router';
+import { Router, RouterConfiguration, RouteConfig } from 'aurelia-router';
 import { PLATFORM } from 'aurelia-pal';
+import { autoinject } from 'aurelia-framework';
 
 export class App {
     router: Router;
     title: string = 'CoachSmart';
 
-    configureRouter(config: RouterConfiguration, router: Router) {
+
+    configureRouter(config: RouterConfiguration, router: Router): void {
+        this.router = router;
         config.title = this.title;
         config.map([
-            { route: '', moduleId: PLATFORM.moduleName('home/home'), title: 'Home' }
+            { route: '', moduleId: PLATFORM.moduleName('home/home'), title: 'Home' },
+            { route: 'roster', moduleId: PLATFORM.moduleName('roster/roster'), title: 'Roster' }
         ]);
 
-        this.router = router;
+        console.log(this.router);
     }
 }
